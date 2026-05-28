bc1ad0838f600617778c49bbaf619d2f49cfa55b
wangdl <wdlzhy@163.com>
2026-05-28 06:22:14 +0000

    新建 PmsProductReview.java

--- mall-mbg/src/main/java/com/macro/mall/model/PmsProductReview.java
+++ mall-mbg/src/main/java/com/macro/mall/model/PmsProductReview.java
@@ -5,29 +5,25 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @TableName("pms_product_review")
-public class PmsProductReview implements Serializable {
+public class PmsProductReview {
+    
     @TableId(type = IdType.AUTO)
     private Long id;
-
+    
     private Long productId;
-
+    
     private Long memberId;
-
-    @Schema(title = "评价星数：1-5")
+    
     private Integer star;
-
+    
     private String content;
-
+    
     @TableField(fill = FieldFill.INSERT)
     private Date createTime;
-
-    private static final long serialVersionUID = 1L;
 }
